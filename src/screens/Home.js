f1b09c5e255f6115e382d60529621c29ba62d64e
Raashul <rashul1996@gmail.com>
2018-10-12 13:18:53 -0400

    add init files again

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+  componentDidMount(){
+    if(process.env.REACT_APP_URL) console.log('testing', process.env.REACT_APP_URL);
+  }
   render() {
     return (
       <div className="App">
